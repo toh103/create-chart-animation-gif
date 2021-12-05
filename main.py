7e35a4e2a18badb356e16126a8c700392bc32660
tohtoh103 <ktmatsu@me.com>
2021-12-05 14:23:38 +0900

    Add yfinance to get stock info

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+import pandas as pd
+import yfinance as yf
+
 
 class Animation():
-    def __init__(self, datasets=[0, ]):
+    def __init__(self, datasets=[]):
         self.datasets = datasets
         # list for animation chart
+        self.x = []
         self.y = []
         self.fig = plt.figure()
         # start animation
@@ -18,22 +22,54 @@ class Animation():
         if len(self.datasets) <= 0:
             # stop animation
             self.anime.event_source.stop()
+            print("Stop animation")
         else:
-            value = self.datasets.pop()
-            self.y.append(value)
+            x, y = self.datasets.pop(0)
+            self.x.append(x)
+            self.y.append(y)
         # show plot
-        plt.plot(self.y)
+        plt.plot(self.x, self.y)
 
     def animate(self):
+        print9 = ("Start animation.")
         self.anime = animation.FuncAnimation(
             self.fig, self.AnimationUpdater, interval=100)
         plt.show()
 
 
+class Stock:
+    def __init__(self, code="", period="7d", interval="15m"):
+        self.code = code
+        self.period = period
+        self.interval = interval
+
+        self.stock_df = self.download_stock_data()
+
+    def download_stock_data(self):
+        print("Downloading stock data ... ")
+        try:
+            stock_df = yf.download(
+                self.code, period=self.period, interval=self.interval)
+        except e:
+            print("Download Error.")
+        print("Complete.")
+        return stock_df
+
+    def get_idx_vs_price_list(self):
+        # ls = [[idx, price], [idx, price] ,...]
+        idx_vs_price = [[0, self.stock_df.iloc[0]["Open"]]]
+        idx = 1
+        for row in self.stock_df[["Open", "Close"]].itertuples():
+            idx_vs_price.append([idx, row.Close])
+            idx = idx + 1
+        return idx_vs_price
+
+
 def main():
-    datasets = [int(np.random.rand() * 20) for i in range(20)]
+    stock = Stock(code="^NDX", period="1d", interval="5m")
+    datasets = stock.get_idx_vs_price_list()
     anime = Animation(datasets)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

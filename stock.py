205f2cf1dcddbdf4a14e9927a541b34294aae513
tohtoh103 <ktmatsu@me.com>
2021-12-06 19:10:39 +0900

    change filename

--- stock.py
+++ stock.py
@@ -7,25 +7,33 @@ import matplotlib.animation as animation
 import pandas as pd
 import yfinance as yf
 
-plt.style.use('dark_background')
+plt.style.use("dark_background")
+# ----------
+#  CONFIG
+# ----------
+font_family = "Monaco"
+color_positive = "springgreen"
+color_negative = "orangered"
 
 
-class Animation():
-    def __init__(self, datasets=[], yesterday_close_price=0):
-        self.datasets = datasets
+class StockAnimation:
+    def __init__(self, stock):
+        self.stock = stock
+        self.datasets = stock.get_idx_vs_price_list()
 
         # list for animation chart
         self.x = []
         self.y = []
         # For calculation
-        self.yesterday_close_price = yesterday_close_price
-        self.price_list = [price for idex, price in datasets]
+        self.yesterday_close_price = stock.get_yesterday_close_price()
+        self.price_list = [price for idex, price in self.datasets]
         self.datasets_num = len(self.datasets)
         self.price_min = min(self.price_list)
         self.price_max = max(self.price_list)
-        self.open_price = float(self.price_list[0])
-        self.current_price = self.open_price
+        self.current_price = float(self.price_list[0])
         self.fluctuation = 0
+
+        print(stock.get_todays_date())
 
         # fig
         self.y_top = self.price_max + \
@@ -36,11 +44,15 @@ class Animation():
         self.fig, self.ax = plt.subplots(figsize=(10, 6))
         self.line, = self.ax.plot([], [])
         self.title = self.ax.set_title(
-            'NASDAQ100 INDEX', fontsize=24, fontfamily="Arial")
+            None, fontsize=24, fontfamily=font_family)
         self.text_info = self.ax.text(
-            1, 1, None, fontsize=12,
-            transform=self.ax.transAxes, va='top', ha="right",
-            fontfamily="Arial", bbox=dict(boxstyle='square', fc=self.get_fluctuation_color()),)
+            1, 0.95, None, fontsize=12, alpha=0.8,
+            transform=self.ax.transAxes, va="center", ha="right",
+            fontfamily=font_family)
+        self.text_rate = self.ax.text(
+            0.5, 0.5, None, fontsize=96,
+            transform=self.ax.transAxes, va="center", ha="center", alpha=0.7,
+            fontfamily=font_family, color='white')
        # start animation
         self.animate()
 
@@ -50,9 +62,9 @@ class Animation():
         self.ax.set_ylim(self.y_bot, self.y_top)
         self.ax.tick_params(labelsize=18,)
         del (self.x[:], self.y[:])
-        self.title.set_text("NASDAQ100 INDEX")
+        self.title.set_text(
+            "NASDAQ100 INDEX ({})".format(self.stock.get_todays_date()))
         self.text_info.set_text(None)
-        self.text_info.set_bbox(dict(fc=self.get_fluctuation_color()))
         self.ax.xaxis.set_visible(False)
         return self.line,
 
@@ -61,13 +73,15 @@ class Animation():
             self.current_price / self.yesterday_close_price - 1)
 
     def str_info(self):
-        return 'Before:${:.2f}, Now:${:.2f}, Rate:{:+.2%}'.format(self.yesterday_close_price, self.current_price, self.fluctuation)
+        return "PREVIOUS CLOSE :${:.2f}\nCURRENT :${:.2f}".format(self.yesterday_close_price, self.current_price)
 
+    def str_rate(self):
+        return "{:.2%}".format(self.fluctuation)
     # def str_info(self):
-    #     return 'Rate:{:.2%}'.format(self.fluctuation)
+    #     return "Rate:{:.2%}".format(self.fluctuation)
 
     def get_fluctuation_color(self):
-        return 'tomato' if self.fluctuation <= 0 else 'seagreen'
+        return color_negative if self.fluctuation <= 0 else color_positive
 
     def update(self, frame):
         # update graph area
@@ -81,7 +95,8 @@ class Animation():
         self.current_price = y
         self.update_fluctuation()
         self.text_info.set_text(self.str_info())
-        self.text_info.set_bbox(dict(fc=self.get_fluctuation_color()))
+        self.text_rate.set_text(self.str_rate())
+        self.text_rate.set_color(self.get_fluctuation_color())
 
         return self.line,
 
@@ -89,11 +104,11 @@ class Animation():
         print("Start animation.")
         self.anime = animation.FuncAnimation(
             self.fig, self.update, frames=range(self.datasets_num), interval=30, init_func=self.init_ax_style, repeat=False)
-        self.anime.save('test-anime.mp4',
-                        writer='ffmpeg')  # fpsはデフォルトの5
+        # self.anime.save("test-anime.mp4",
+        #                 writer="ffmpeg")  # fpsはデフォルトの5
 
-        # plt.show()
-        # plt.close()
+        plt.show()
+        plt.close()
 
 
 class Stock:
@@ -101,16 +116,12 @@ class Stock:
         self.code = code
         self.period = period
         self.interval = interval
-
+        # 1st. 1d interval stock df
+        # 2nd. 2m interval stock df for target-ymd
+        self.oneday_interval_stock_df = self.download_stock_data_from_period(
+            code=code, period="7d", interval="1d")
         self.todays_stock_df = self.download_stock_data_from_period(
             code, period, interval)
-        # get todays close price(1m stock_df has wrong data)
-        self.todays_stock_df_1d = self.download_stock_data_from_period(
-            code, "1d", "1d")
-        # get yesterdays stock_df
-        cur_date, one_week_ago = self.get_date_x_ago(0), self.get_date_x_ago(7)
-        self.to_yesterdays_stock_df_1d = self.download_stock_data_from_date(
-            code, one_week_ago, cur_date, "1d")
 
     def download_stock_data_from_period(self, code, period, interval):
         print("Downloading stock data ... ")
@@ -132,20 +143,14 @@ class Stock:
         print("Complete.")
         return stock_df
 
-    def get_date_x_ago(self, ago=0):
-        cur_datetime = self.todays_stock_df.index[0]
-        ago_datetime = cur_datetime - datetime.timedelta(days=ago)
+    def get_yesterday_close_price(self):
+        return float(self.oneday_interval_stock_df.iloc[-2]["Close"])
 
-        cur_date_str = cur_datetime.strftime("%Y-%m-%d")
-        ago_datetime_str = ago_datetime.strftime("%Y-%m-%d")
-        return ago_datetime_str
+    def get_todays_close_price(self):
+        return float(self.oneday_interval_stock_df.iloc[-1]["Close"])
 
-    def get_yesterday_last_price(self):
-
-        return float(self.to_yesterdays_stock_df_1d.iloc[-1:]["Close"])
-
-    def get_todays_last_price(self):
-        return float(self.todays_stock_df_1d.iloc[0]["Close"])
+    def get_todays_date(self):
+        return self.todays_stock_df.index[0].strftime("%Y/%m/%d")
 
     def get_idx_vs_price_list(self):
         # ls = [[idx, price], [idx, price] ,...]
@@ -154,17 +159,18 @@ class Stock:
         for row in self.todays_stock_df[["Open", "Close"]].itertuples():
             idx_vs_price.append([idx, row.Close])
             idx = idx + 1
-        idx_vs_price.append([idx, self.get_todays_last_price()])
-        print(idx, self.get_todays_last_price())
+        idx_vs_price.append([idx, self.get_todays_close_price()])
+        print(idx, self.get_todays_close_price())
         return idx_vs_price
+
+
+def get_recent_workday():
+    return datetime.datetime.now().strftime("%Y-%M-$d")
 
 
 def main():
     stock = Stock(code="^NDX", period="1d", interval="2m")
-    datasets = stock.get_idx_vs_price_list()
-    yesterday_close_price = stock.get_yesterday_last_price()
-
-    anime = Animation(datasets, yesterday_close_price)
+    anime = StockAnimation(stock)
 
 
 if __name__ == "__main__":
